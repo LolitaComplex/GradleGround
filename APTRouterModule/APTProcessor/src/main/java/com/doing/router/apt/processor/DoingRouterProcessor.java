480f46fa5c8945ffea1ca28a095525743756f7c9
杜营 <duying@inke.cn>
2021-11-01 20:27:48 +0800

    新增: 添加调用逻辑

--- APTRouterModule/APTProcessor/src/main/java/com/doing/router/apt/processor/DoingRouterProcessor.java
+++ APTRouterModule/APTProcessor/src/main/java/com/doing/router/apt/processor/DoingRouterProcessor.java
@@ -1,18 +1,26 @@
 package com.doing.router.apt.processor;
 
 import com.doing.router.apt.annotations.DoingRouter;
+import com.google.auto.service.AutoService;
 
 import java.util.Collections;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 
+@AutoService(Processor.class)
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class DoingRouterProcessor extends AbstractProcessor {
 
     private static final String TAG = "DoingRouterProcessor";
+    public static final String CLASS_NAME = DoingRouter.class.getCanonicalName();
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
@@ -47,11 +55,13 @@ public class DoingRouterProcessor extends AbstractProcessor {
         }
 
         System.out.println(TAG + " >>> process finish. ");
-        return false;
+        return true;
     }
 
     @Override
     public Set<String> getSupportedAnnotationTypes() {
+        System.out.println(TAG + " >>> support type: " + DoingRouter.class.getCanonicalName());
         return Collections.singleton(DoingRouter.class.getCanonicalName());
     }
+
 }
